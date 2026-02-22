781ac85540aec6e5f4eb8028c72c16d7521bebcc
Lil-ice-Cloud <workformy110@gmail.com>
2026-02-22 15:53:29 +0530

    sidenavbar update

---
+++ src/app/money/money.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-money',
+  standalone: true,
+  imports: [],
+  templateUrl: './money.html',
+  styleUrl: './money.css',
+})
+
+export class Money {}
